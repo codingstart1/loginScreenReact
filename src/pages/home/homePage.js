6b53c9ae6d5d370d5db145c3185ecedb34815aaf
MoustafaYasserGit <MOU.fci15@gmail.com>
2023-09-12 21:55:15 +0400

    homepage design

--- src/pages/home/homePage.js
+++ src/pages/home/homePage.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React from 'react';
+import userAvatar from '../../assets/images/user-avatar.png'
+import { useHistory } from 'react-router-dom';
 
 export default function HomePage() {
+  const history = useHistory()
   return (
-    <div>Home page</div>
+    <div className='home-screen'>
+      <header>
+        <h3>System Name</h3>
+        <a onClick={()=> history.push('/')}>Logout</a>
+      </header>
+      <div className='page-content'>
+        <div className='user-card'>
+          <img src={userAvatar} alt='user-avatar' />
+          <h3>User Full Name</h3>
+          <div className='user-meta'>
+            <p>
+              <span>Username:</span>
+              <span>Test user name</span>
+            </p>
+            <p>
+              <span>Email:</span>
+              <span>test@test.com</span>
+            </p>
+            <p>
+              <span>Gender:</span>
+              <span>Male or Female</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/pages/login/loginPage.js
+++ src/pages/login/loginPage.js
@@ -1,33 +1,35 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export default function LoginPage() {
-  return (
-    <div className='login-screen'>
-        <div className='login-form'>
-            <div className='left-part' />
-            <div className='right-part'>
-                <h3>Login Account</h3>
-                <div className='input-group'>
-                    <label>Username</label>
-                    <input
-                        type='text'
-                        className='form-control'
-                        placeholder='Enter username'
-                    />
-                </div>
-                <div className='input-group'>
-                    <label>Password</label>
-                    <input
-                        type='password'
-                        className='form-control'
-                        placeholder='Enter password'
-                    />
-                </div>
-                <div className='submit-action'>
-                    <button className='submit-btn' onClick={()=>console.log("Login BTN")}>Login</button>
+    const history = useHistory();
+    return (
+        <div className='login-screen'>
+            <div className='login-form'>
+                <div className='left-part' />
+                <div className='right-part'>
+                    <h3>Login Account</h3>
+                    <div className='input-group'>
+                        <label>Username</label>
+                        <input
+                            type='text'
+                            className='form-control'
+                            placeholder='Enter username'
+                        />
+                    </div>
+                    <div className='input-group'>
+                        <label>Password</label>
+                        <input
+                            type='password'
+                            className='form-control'
+                            placeholder='Enter password'
+                        />
+                    </div>
+                    <div className='submit-action'>
+                        <button className='submit-btn' onClick={()=> history.push('/home')}>Login</button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
