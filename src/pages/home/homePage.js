dab0900199f746594a0f9f0ca79cd45ab739e4ca
MoustafaYasserGit <MOU.fci15@gmail.com>
2023-09-13 00:21:02 +0400

    finalize project

--- src/pages/home/homePage.js
+++ src/pages/home/homePage.js
@@ -1,34 +1,44 @@
-import React from 'react';
-import userAvatar from '../../assets/images/user-avatar.png'
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
-export default function HomePage() {
-  const history = useHistory()
+export default function HomePage(props) {
+  const history = useHistory();
+
+  // we need to fetch userData from history state and set it into our home page
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    setUserData(props.location.state.userData);
+  }, [props.location.userData]);
+
+
   return (
     <div className='home-screen'>
       <header>
         <h3>System Name</h3>
-        <a onClick={()=> history.push('/')}>Logout</a>
+        <a onClick={() => history.push('/')}>Logout</a>
       </header>
       <div className='page-content'>
-        <div className='user-card'>
-          <img src={userAvatar} alt='user-avatar' />
-          <h3>User Full Name</h3>
-          <div className='user-meta'>
-            <p>
-              <span>Username:</span>
-              <span>Test user name</span>
-            </p>
-            <p>
-              <span>Email:</span>
-              <span>test@test.com</span>
-            </p>
-            <p>
-              <span>Gender:</span>
-              <span>Male or Female</span>
-            </p>
+        {userData && (
+          <div className='user-card'>
+            <img src={userData.image} alt='user-avatar' />
+            <h3>{userData.firstName}{' '}{userData.lastName}</h3>
+            <div className='user-meta'>
+              <p>
+                <span>Username:</span>
+                <span>{userData.username}</span>
+              </p>
+              <p>
+                <span>Email:</span>
+                <span>{userData.email}</span>
+              </p>
+              <p>
+                <span>Gender:</span>
+                <span>{userData.gender}</span>
+              </p>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   )
--- src/pages/login/loginPage.js
+++ src/pages/login/loginPage.js
@@ -1,8 +1,72 @@
-import React from 'react'
-import { useHistory } from 'react-router-dom'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import LoaderComponent from '../../helpers/loader/loaderComponent';
+
 
 export default function LoginPage() {
     const history = useHistory();
+
+    const [userName, setUserName] = useState('');
+    const [userPassword, setUserPassword] = useState('');
+    const [loader, setShowHideLoader] = useState(false);
+
+    const formValidation = () => {
+        let isValid = true;
+
+        if (!userName) {
+            toast.warn('Username is required');
+            isValid = false
+        }
+
+        if (!userPassword) {
+            toast.warn('User password is required');
+            isValid = false
+        }
+
+        return isValid
+    }
+
+    const submitUserLogin = async () => {
+        try {
+            // before submit, we need to check validation
+            if (formValidation()) {
+
+                // we need to add loading spinner to wait server to response.
+                // let create loader component
+                setShowHideLoader(true);
+                const reqBody = {
+                    "username": userName,
+                    "password": userPassword
+                }
+                const response = await axios.post('https://dummyjson.com/auth/login', reqBody);
+                if (response) {
+
+                    // here we need to store data as temp and navigate to home page
+                    history.push({
+                        pathname: '/home',
+                        state: { userData: response.data }
+                    });
+
+                    // now we navigated to home but with static data.
+                    // let us go to home page.
+                    // data will go to home page as props.
+                }
+            }
+
+        }
+        catch (e) {
+            // log error in case of invalid user login username and password
+            // we will use Toaster to show the error to user
+            toast.error(e.response.data.message);
+        }
+        finally {
+            setShowHideLoader(false);
+        }
+    }
+
     return (
         <div className='login-screen'>
             <div className='login-form'>
@@ -15,6 +79,8 @@ export default function LoginPage() {
                             type='text'
                             className='form-control'
                             placeholder='Enter username'
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
                         />
                     </div>
                     <div className='input-group'>
@@ -23,13 +89,17 @@ export default function LoginPage() {
                             type='password'
                             className='form-control'
                             placeholder='Enter password'
+                            value={userPassword}
+                            onChange={(e) => setUserPassword(e.target.value)}
                         />
                     </div>
                     <div className='submit-action'>
-                        <button className='submit-btn' onClick={()=> history.push('/home')}>Login</button>
+                        <button className='submit-btn' onClick={() => submitUserLogin()}>Login</button>
                     </div>
                 </div>
             </div>
+            <ToastContainer />
+            {loader && <LoaderComponent />}
         </div>
     )
 }
